23fdedd7d65a691315862ffba5f71447a61ed950
Hardik Ruparel <hardik@Hardiks-MacBook-Pro.local>
2022-04-26 11:05:18 -0700

    Adding start date and end date for create Event

--- socialseriveapp/src/app/dashboard/dashboard.component.ts
+++ socialseriveapp/src/app/dashboard/dashboard.component.ts
@@ -15,6 +15,7 @@ import { DashboardService } from '../users/services/dashboardservice/dashboard.s
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
   user!: User;
   isUser!: boolean;
@@ -32,9 +33,12 @@ export class DashboardComponent implements OnInit {
   eventCity: string="";
   eventZip: string="";
   searchEventsQuery: string="";
-  searchQueryObject!:SearchQuery; 
+  searchQueryObject!:SearchQuery;
+  startDate!: Date;
+  endDate!: Date;
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
+  events!: Event;
   constructor(private dashboardService: DashboardService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
@@ -55,8 +59,12 @@ export class DashboardComponent implements OnInit {
       phoneNo: Number(this.eventPOCContact),
       email: this.eventPOCEmail,
       city: this.eventCity,
-      pinCode: Number(this.eventZip)
+      pinCode: Number(this.eventZip),
+      startDate: this.startDate,
+      endDate: this.endDate
     }
+
+    console.log(this.createEventObject);
     
     this.dashboardService.createEvent(this.createEventObject).subscribe((res)=>{
       this._snackBar.open('Event Created!!', "",{
@@ -81,7 +89,7 @@ export class DashboardComponent implements OnInit {
     }
     console.log(this.searchQueryObject);
     this.dashboardService.searchEvents(this.searchQueryObject).subscribe((res)=>{
-      console.log(res)
+      console.log(res);
       this.searchEventsQuery="";
     },(err)=>{
       this.searchEventsQuery="";
